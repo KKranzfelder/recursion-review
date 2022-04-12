2f2307795e27c7d1dc343c4e71f6cf0aa5e36087
Josue Bejar <josuebejar11@gmail.com>
2022-04-11 21:57:06 -0400

    Finish getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,35 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// You should use document.body, element.childNodes, and element.classList
+//classList returns arraylike of classes
+//chidlNodes returns arraylike of childs
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+
+//I:string or array of strings
+//0: an array of elements containing className
+//C: cannot use native function getElementsByClassName
+//E:
+
+var getElementsByClassName = function(className, curElement
 ) {
-  // your code here
+
+  if (!curElement) {
+    curElement = document.body;
+  }
+  var result = [];
+  var classes = className.split(' ');
+
+  _.each(classes, function (curClass) {
+    if (curElement.classList.contains(curClass)) {
+      result.push(curElement);
+    }
+  });
+
+  if (curElement.children.length !== 0) {
+    _.each(curElement.children, function (child) {
+      result = result.concat(getElementsByClassName(className, child));
+    });
+  }
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,89 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+//I: object, number, array, null
+//O: string
+//C: undefined, functions
+//E:
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  var isObject = typeof obj === 'object' && obj !== null;
+
+  var stringValue = function(value) {
+    if (typeof value === 'string') {
+      result += '"' + value + '"';
+    }
+    if (typeof value === 'number' && !isNaN(value)) {
+      result += value.toString();
+    }
+    if (typeof value === 'boolean') {
+      value ? result += 'true' : result += 'false';
+    }
+    if (typeof value === 'object' && value === null) {
+      result += 'null';
+    }
+    if (typeof value === 'function' || typeof value === 'undefined') {
+      result += '';
+    }
+  };
+
+  stringValue(obj);
+
+  if (isObject) {
+    if (Array.isArray(obj)) {
+      result += '[';
+      _.each(obj, function (curValue, index) {
+        if (typeof curValue === 'object' && curValue !== null) {
+          result += stringifyJSON(curValue);
+        } else {
+          stringValue(curValue);
+        }
+        if (obj.length - 1 !== index) {
+          result += ',';
+        }
+      });
+      result += ']';
+    } else {
+      result += '{';
+      var keys = Object.keys(obj);
+      _.each(keys, function(key, index) {
+        if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+          stringValue(key);
+          result += ':';
+          if (typeof obj[key] === 'object' && obj[key] !== null) {
+            result += stringifyJSON(obj[key]);
+          }
+          stringValue(obj[key]);
+          if (keys[keys.length - 1] !== key) {
+            result += ',';
+          }
+        }
+        /*stringValue(key);
+        result += ':';
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          result += stringifyJSON(obj[key]);
+        }
+        stringValue(obj[key]);
+        if (keys[keys.length - 1] !== key) {
+          result += ',';
+        }
+        */
+      });
+      /*for (key in obj) {
+        stringValue(key);
+        result += ':';
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          result += stringifyJSON(obj[key]);
+        }
+        stringValue(obj[key]);
+        if (key !== _.keys(obj)[obj.length - 1]) {
+          result += ',';
+        }
+      }*/
+      result += '}';
+    }
+  }
+  return result;
 };
